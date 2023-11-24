f4f96b2de93276a76acbad553a4facecd540283d
HajMichal <michalhaj2003@gmail.com>
2023-11-24 14:18:08 +0100

    [refactor]: change way to upload and save images

--- src/pages/aktualnosci/index.tsx
+++ src/pages/aktualnosci/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Loading, Navbar, SideBar } from "~/components";
 import {
   TextInput,
@@ -7,6 +7,10 @@ import {
   Button,
   Tooltip,
   FileInput,
+  Card,
+  Image,
+  Text,
+  Group,
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { PiPlusBold } from "react-icons/pi";
@@ -15,31 +19,19 @@ import { useSession } from "next-auth/react";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import { api } from "~/utils/api";
 import toast, { Toaster } from "react-hot-toast";
-import { sendImageToS3 } from "~/utils/sendImageToS3";
-
+import { useUploadThing } from "~/utils/uploadthing";
+import { FileWithPath } from "@uploadthing/react";
 interface PostData {
   title: string;
   description: string;
-}
-export interface FileData {
-  fields: FieldsType;
   url: string;
-}
-interface FieldsType {
-  Policy: string;
-  "X-Amz-Algorithm": string;
-  "X-Amz-Credential": string;
-  "X-Amz-Date": string;
-  "X-Amz-Signature": string;
-  bucket: string;
 }
 
 const Aktualnosci = () => {
   const [opened, { open, close }] = useDisclosure(false);
   const { data: sessionData } = useSession();
-  const [file, setFile] = useState<null | File>(null);
-  const [isEnabled, setIsEnabled] = useState(false);
-
+  const [file, setFile] = useState<File[]>([]);
+  const [url, setUrl] = useState<string>();
   const { register, handleSubmit, watch } = useForm<PostData>({
     defaultValues: {
       title: "",
@@ -47,32 +39,39 @@ const Aktualnosci = () => {
     },
   });
 
-  const {
-    data: predesignedData,
-    isSuccess,
-    isError,
-  } = api.newsDataRouter.createPredesignedUrl.useQuery<FileData>(
-    { description: watch("description"), title: watch("title") },
-    { enabled: isEnabled }
-  );
+  const { mutate } = api.newsDataRouter.createNewPost.useMutation();
+  const { data } = api.newsDataRouter.getLastPosts.useQuery();
+  console.log(data);
+  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+    setFile(acceptedFiles);
+  }, []);
 
-  const onSubmit: SubmitHandler<PostData> = () => {
+  const { startUpload, isUploading } = useUploadThing("imageUploader", {
+    onClientUploadComplete: (data) => {
+      setUrl(data[0]?.url);
+      toast.success("Poprawnie zapisano obraz");
+    },
+    onUploadError: () => {
+      toast.error("Zły obraz, spróbuj ponownie później lub wybierz inny obraz");
+    },
+  });
+
+  const onSubmit: SubmitHandler<PostData> = async () => {
     if (file) {
-      setIsEnabled(true);
-      close();
+      await startUpload(file);
     }
   };
 
   useEffect(() => {
-    sendImageToS3(predesignedData, file)
-      .then()
-      .catch(() =>
-        toast.error(
-          "Wystąpił bład. Post nie został dodany. Spróbuj ponownie później."
-        )
-      );
-    setIsEnabled(false);
-  }, [file, isSuccess, isError]);
+    if (!isUploading && url) {
+      mutate({
+        title: watch("title"),
+        description: watch("description"),
+        url: url,
+      });
+      close();
+    }
+  }, [isUploading, url]);
 
   return (
     <main className="flex h-full max-h-screen justify-center overflow-hidden bg-backgroundGray font-orkney">
@@ -103,41 +102,70 @@ const Aktualnosci = () => {
             size="50%"
             className="font-orkneyBold"
           >
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="flex flex-col items-center p-10"
-            >
-              <TextInput
-                {...register("title")}
-                label="Tytuł"
-                type="text"
-                required
-                className="w-[90%]"
-              />
-              <Textarea
-                {...register("description")}
-                label="Opis"
-                required
-                className="w-[90%]"
-              />
-              <div className="flex w-full items-end justify-center gap-10">
-                <FileInput
+            {isUploading ? (
+              <Loading />
+            ) : (
+              <form
+                onSubmit={handleSubmit(onSubmit)}
+                className="flex flex-col items-center p-10"
+              >
+                <TextInput
+                  {...register("title")}
+                  label="Tytuł"
+                  type="text"
                   required
-                  rightSection={<MdOutlineAttachFile />}
-                  label="Dołącz plik"
-                  onChange={(e) => e && setFile(e)}
-                  // rightSectionPointerEvents="none"
-                  className="w-[50%]"
-                  mt="md"
+                  className="w-[90%]"
                 />
-                <button className="font-orkneyBoldg font-orkneyBold- w-[33%] rounded-lg bg-brand p-2 px-5 text-lg text-dark duration-100 hover:scale-105 hover:bg-brand hover:opacity-70">
-                  Dodaj
-                </button>
-              </div>
-            </form>
+                <Textarea
+                  {...register("description")}
+                  label="Opis"
+                  required
+                  className="w-[90%]"
+                />
+                <div className="flex w-full items-end justify-center gap-10">
+                  <FileInput
+                    required
+                    rightSection={<MdOutlineAttachFile />}
+                    label="Dołącz plik"
+                    onChange={(e) => e && setFile([e])}
+                    // rightSectionPointerEvents="none"
+                    className="w-[50%]"
+                    mt="md"
+                  />
+
+                  <button className="font-orkneyBoldg font-orkneyBold- w-[33%] rounded-lg bg-brand p-2 px-5 text-lg text-dark duration-100 hover:scale-105 hover:bg-brand hover:opacity-70">
+                    Dodaj
+                  </button>
+                </div>
+              </form>
+            )}
           </Modal>
         </div>
-        {/* <div>{isLoading && <Loading />}</div> */}
+        <div className="flex flex-1 flex-wrap justify-center gap-5 p-10">
+          {/* {imagesWithData?.map((imageWithData) => {
+            return (
+              <Card
+                shadow="sm"
+                padding="lg"
+                radius="md"
+                withBorder
+                className="max-w-md"
+              >
+                <Card.Section>
+                  <Image src={imageWithData.url} height={160} alt="image" />
+                </Card.Section>
+
+                <Group mt="md" mb="xs">
+                  <Text fw={500}>{imageWithData.title}</Text>
+                </Group>
+
+                <Text size="sm" c="dimmed">
+                  {imageWithData.description}
+                </Text>
+              </Card>
+            );
+          })} */}
+        </div>
       </div>
     </main>
   );
--- src/server/api/routers/newsData.ts
+++ src/server/api/routers/newsData.ts
@@ -1,75 +1,27 @@
 import { z } from "zod";
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
-import { Image } from "@prisma/client";
-import { PutObjectCommand } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { s3 } from "../routers/photovoltaic/dataFlow";
-interface FileData {
-  fields: Record<string, string>;
-  url: string;
-}
-interface ImageMetadata extends Image {
-  url: string;
-}
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
 export const newsDataRouter = createTRPCRouter({
-  createPredesignedUrl: publicProcedure
+  createNewPost: publicProcedure
     .input(
       z.object({
         title: z.string(),
         description: z.string(),
+        url: z.string(),
       })
     )
-    .query(async ({ ctx, input }) => {
-      if (!ctx.session) {
-        throw new Error("Musisz być zalogowany");
-      }
-
-      const image = await ctx.prisma.image.create({
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.prisma.image.create({
         data: {
           title: input.title,
           description: input.description,
+          url: input.url,
         },
       });
-      return new Promise((resolve, reject) => {
-        s3.createPresignedPost(
-          {
-            Bucket: "ridearem",
-            Fields: {
-              key: `images/${image.id}`,
-            },
-            Conditions: [
-              ["starts-with", "$Content-Type", "image/"],
-              ["content-length-range", 0, 2000000],
-            ],
-            Expires: 60,
-          },
-          (err: Error, signed: FileData) => {
-            if (err) return reject(err);
-            resolve(signed);
-          }
-        );
-      });
     }),
-  getImagesFromS3: publicProcedure.query(async ({ ctx }) => {
-    const imagesWithData = await ctx.prisma.image.findMany({
+  getLastPosts: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.image.findMany({
       take: 6,
     });
-
-    const extendedImages: ImageMetadata[] = await Promise.all(
-      imagesWithData.map(async (image) => {
-        return {
-          ...image,
-          url: await s3.getSignedUrlPromise("getObject", {
-            Bucket: "ridearem",
-            Key: `images/${image.id}`,
-          }),
-        };
-      })
-    );
-    return extendedImages;
   }),
 });
