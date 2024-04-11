ac588a3da803735398c767e9f87beadb591014d3
HajMichal <michalhaj2003@gmail.com>
2024-04-11 15:24:39 +0200

    [minor]: changed way how to update aws s3 keys and region

--- src/utils/aws/config.ts
+++ src/utils/aws/config.ts
@@ -1,11 +1,15 @@
 import AWS from "aws-sdk";
 
-export const s3 = new AWS.S3();
-export const bucket = process.env.BUCKET!;
-export const prefix = "documents";
-
-AWS.config.update({
+export const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   region: "eu-central-1",
 });
+export const bucket = process.env.BUCKET!;
+export const prefix = "documents";
+
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: "eu-central-1",
+// });
