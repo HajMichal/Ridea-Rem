718044540563247ce17c4ffe51fd4b9e67af9af1
HajMichal <michalhaj2003@gmail.com>
2024-04-18 19:04:47 +0200

    [refactor]: changed design of /createAccount subpage

--- src/pages/stworz/konto.tsx
+++ src/pages/stworz/konto.tsx
@@ -4,7 +4,8 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { api } from "~/utils/api";
 import { Toaster, toast } from "react-hot-toast";
-import { SideBar } from "~/components/LazyLoading";
+import Image from "next/image";
+import { PasswordInput, Radio, TextInput } from "@mantine/core";
 import { Navbar } from "~/components";
 
 interface FormTypes {
@@ -28,7 +29,7 @@ const Account = () => {
   const { mutate: addMenagerPhotovoltaicData } =
     api.dataFlow.addNewMenager.useMutation({
       onSuccess(data) {
-        toast.success(data!.message, { duration: 7000 });
+        toast.success(data!.message, { duration: 4000 });
       },
     });
   const { mutate: addMenagerHeatPumpData } =
@@ -76,67 +77,107 @@ const Account = () => {
   };
 
   return (
-    <div className="flex h-full max-h-screen min-h-screen justify-center bg-[#E8E7E7] font-orkney">
-      <SideBar />
-      <div className="flex max-h-screen w-full flex-wrap justify-center">
-        <Navbar />
-        <div className="h-full max-h-[80%]">
-          <h1 className="mb-4 w-full  text-center text-xl font-medium text-dark">
-            Stwórz nowe konto
-          </h1>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex w-min max-w-[350px] flex-wrap justify-center gap-3 text-dark"
-          >
-            <input
-              {...register("name", { required: true })}
-              autoComplete={"off"}
-              placeholder="Imię"
-              className="p-1"
+    <div className="h-screen w-screen overflow-hidden bg-[#E8E7E7] font-orkney">
+      <Navbar />
+      <div className="flex h-full w-full justify-center bg-[url(/login.png)] bg-cover">
+        <div className="mt-10 h-fit w-5/6 max-w-[700px] rounded-[60px] bg-white bg-opacity-90 p-10 shadow-sm ">
+          <div className="flex w-full items-center justify-center ">
+            <Image
+              src={"/icons/logo_solo_color.svg"}
+              alt="Logo"
+              width={80}
+              height={80}
+              className="lg:h-[10%] lg:w-[10%] xl:h-1/6 xl:w-1/6"
             />
-            <input
-              {...register("login", { required: true })}
-              autoComplete={"off"}
-              placeholder="Login"
-              className="p-1"
-            />
-            <input
-              {...register("password", { required: true })}
-              autoComplete={"off"}
-              type="password"
-              placeholder="Hasło"
-              className="p-1"
-            />
-            {sessionData?.user.role === 1 && (
-              <>
-                <p className="w-full text-center">Ranga</p>
-                <div className="flex gap-1">
-                  <label htmlFor="role">Menager</label>
-                  <input
-                    {...register("role", { required: true })}
-                    type="radio"
-                    name="role"
-                    value={2}
-                  />
-                </div>
-                <div className="flex gap-1">
-                  <input
-                    {...register("role", { required: true })}
-                    type="radio"
-                    name="role"
-                    value={3}
-                  />
-                  <label htmlFor="role">Pracownik</label>
-                </div>
-              </>
-            )}
-            <button
-              type="submit"
-              className="rounded-full bg-brand px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              Stwórz
-            </button>
-          </form>
+          </div>
+          <div className="flex w-full flex-col items-center gap-3 p-10">
+            <h1 className="font-orkneyBold font-extrabold laptop:text-xl xl:text-3xl">
+              DZIEŃ DOBRY!
+            </h1>
+            <h3 className="text-xl">Tutaj stworzysz nowe konto!</h3>
+          </div>
+          <div className="-mt-5 flex w-full justify-center">
+            <div className="flex w-3/5 max-w-sm flex-col gap-5 font-orkneyLight">
+              <TextInput
+                {...register("name", { required: true })}
+                autoComplete={"off"}
+                placeholder="Wpisz tutaj..."
+                className="p-1"
+                label="Imię"
+                maw={620}
+                styles={{
+                  label: {
+                    font: "inherit",
+                  },
+                }}
+              />
+              <TextInput
+                {...register("login", { required: true })}
+                autoComplete={"off"}
+                placeholder="Wpisz tutaj..."
+                className="p-1"
+                label="Login"
+                maw={620}
+                styles={{
+                  label: {
+                    font: "inherit",
+                  },
+                }}
+              />
+              <PasswordInput
+                {...register("password", { required: true })}
+                autoComplete={"off"}
+                type="password"
+                placeholder="Wpisz tutaj..."
+                className="p-1"
+                label="Hasło"
+                maw={620}
+                styles={{
+                  label: {
+                    font: "inherit",
+                  },
+                  visibilityToggle: {
+                    color: "#303030",
+                  },
+                }}
+              />
+              {sessionData?.user.role === 1 && (
+                <>
+                  <p className="-mb-3 w-full text-center">Ranga</p>
+                  <div className="flex justify-center gap-10">
+                    <div className="flex gap-1">
+                      <Radio
+                        {...register("role", { required: true })}
+                        name="role"
+                        value={2}
+                        labelPosition="left"
+                        label="Menager"
+                      />
+                    </div>
+                    <div className="flex gap-1">
+                      <Radio
+                        {...register("role", { required: true })}
+                        name="role"
+                        value={3}
+                        defaultChecked
+                        label="Pracownik"
+                      />
+                    </div>
+                  </div>
+                </>
+              )}
+
+              <button
+                onClick={handleSubmit(onSubmit)}
+                className="mt-4 rounded-full bg-brand p-3 font-orkneyBold"
+              >
+                ZALOGUJ SIĘ
+              </button>
+            </div>
+          </div>
+          <div className="mt-10 flex w-full items-end justify-center p-5 font-orkneyBold xl:mt-20">
+            <h2>IDEA REM © 2024</h2>
+          </div>
         </div>
       </div>
       <Toaster />
