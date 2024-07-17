ea4432c5989c7f2384b2de535ba78c303eb2e5e0
HajMichal <michalhaj2003@gmail.com>
2024-07-17 19:02:40 +0200

    [feat]: start creating airCondition pdf

--- src/components/calculators/airCondition/CreatePDF.tsx
+++ src/components/calculators/airCondition/CreatePDF.tsx
@@ -7,6 +7,11 @@ import {
   StyleSheet,
   Image,
   Font,
+  Defs,
+  RadialGradient,
+  Rect,
+  Stop,
+  Svg,
 } from "@react-pdf/renderer";
 import { type AirConditionStore } from "~/store/airCondition/airConditionSlice";
 import { type AirConditionCalculationStore } from "~/store/airCondition/airConditionCalculationSlice";
@@ -29,104 +34,55 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF",
     fontFamily: "Orkney",
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
-  },
-  pricingSection: {
-    margin: 10,
-    padding: 10,
-    paddingLeft: 50,
-    paddingTop: 50,
-    flexGrow: 1,
-  },
   image: {
     width: "auto",
     height: "auto",
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: 600,
-    fontFamily: "Orkney",
-    marginTop: -30,
-  },
-  subTitle: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-  eachRow: {
-    paddingHorizontal: 20,
-    backgroundColor: "#FEEB1A",
-    borderBottomLeftRadius: "50%",
-    borderTopRightRadius: "50%",
-    padding: 12,
-    fontSize: 12,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "100%",
-    marginVertical: 3,
-    flexDirection: "row",
-  },
-  priceRow: {
-    paddingHorizontal: 20,
-    backgroundColor: "#FEEB1A",
-    borderBottomLeftRadius: "50%",
-    borderTopRightRadius: "50%",
-    padding: 10,
-    fontSize: 12,
-    display: "flex",
-    width: "75%",
-    marginVertical: 3,
-    justifyContent: "space-between",
-    alignItems: "center",
-    flexDirection: "row",
-    marginTop: 50,
-  },
-  paymentReturnTime: {
-    paddingHorizontal: 20,
-    backgroundColor: "#FEEB1A",
-    borderBottomLeftRadius: "50%",
-    borderTopRightRadius: "50%",
-    padding: 10,
-    fontSize: 12,
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "75%",
-    marginVertical: 3,
-    flexDirection: "row",
   },
   boldFont: {
     fontWeight: 600,
     fontFamily: "Orkney",
   },
-  imageSection: {
-    width: 100,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-  },
   logoImage: {
-    width: 50,
-    height: 50,
+    width: 150,
+    height: "auto",
     marginTop: 50,
     marginLeft: 10,
   },
-  signatureImage: {
-    transform: "rotate(90deg)",
-    width: 250,
-    height: "auto",
-    marginLeft: -90,
-    marginTop: -310,
+  mainHeader: {
+    width: "85%",
+    height: 40,
+    marginBottom: 10,
+    marginTop: 10,
+    backgroundColor: "#d1db5a",
+    borderRadius: "20px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  pageNum: {
-    fontSize: 30,
+  mainHeaderText: {
+    fontSize: 22,
+    color: "#FFF",
     fontWeight: 600,
-    marginBottom: 30,
     fontFamily: "Orkney",
-    textAlign: "center",
+    letterSpacing: 1.2,
+    marginBottom: -10,
+  },
+  elementSection: {
+    width: "85%",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: -10,
+  },
+  elementName: {
+    fontFamily: "Orkney",
+    fontSize: 12,
+  },
+  elementValue: {
+    fontFamily: "Orkney",
+    fontWeight: 600,
+    fontSize: 12,
   },
 });
 
@@ -145,7 +101,44 @@ const AirConditionDocument = ({
         src={`${process.env.NEXT_PUBLIC_BASE_URL}/static/airCondition/airConditionFirstPage.png`}
       />
     </Page>
-    <Page size="A4" style={styles.page}></Page>
+    <Page size="A4" style={styles.page}>
+      <View
+        style={{
+          display: "flex",
+          width: "100%",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          style={styles.logoImage}
+          src={`${process.env.NEXT_PUBLIC_BASE_URL}/static/pdf/logoprzyjazna.png`}
+        />
+        <View style={styles.mainHeader}>
+          <Text style={styles.mainHeaderText}>DANE TECHNICZNE</Text>
+        </View>
+        <View style={styles.elementSection}>
+          <Text style={styles.elementName}>TYP KLIMATYZATORA:</Text>
+          <Text style={styles.elementValue}>
+            {airConditionStore.choosedAirConditioner?.type}
+          </Text>
+        </View>
+        <View style={styles.mainHeader}>
+          <Text style={styles.mainHeaderText}>ELEMENTY MONTAŻOWE</Text>
+        </View>
+        <View style={styles.mainHeader}>
+          <Text style={styles.mainHeaderText}>PODSUMOWANIE</Text>
+        </View>
+      </View>
+      <Image
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          zIndex: -500,
+        }}
+        src={`${process.env.NEXT_PUBLIC_BASE_URL}/static/pdf/background.png`}
+      />
+    </Page>
   </Document>
 );
 
