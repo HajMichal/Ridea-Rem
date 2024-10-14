11b91a5dc2e871b7c76ec03fbb09e4a241a6fffa
HajMichal <michalhaj2003@gmail.com>
2024-10-14 10:41:49 +0200

    Fix query data only if menager details are visible

--- src/components/provision/UserEmployees.tsx
+++ src/components/provision/UserEmployees.tsx
@@ -1,76 +1,89 @@
 import { api } from "~/utils/api";
 import type { FeesNames, UserProvisionType } from "./types";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 import { roleNameMapping } from "./UserCard";
 import { ProvisionTableRow } from "./ProvisionTableRow";
 import { avivableProvision } from "./CardBody";
 import { Loading } from "../Loading";
+import { useIntersection } from "~/hooks/useIntersection";
 interface UserEmployeesType {
   user: UserProvisionType;
 }
+
 export const UserEmployees = ({ user }: UserEmployeesType) => {
   const ref = useRef<HTMLDivElement>(null);
-  const isVisible = ref.current?.checkVisibility() === false;
+  const isVisible = useIntersection(ref, "0px");
 
-  const { data, isLoading } = api.userDataHandling.getUsers.useQuery(
+  const { data, isLoading, refetch } = api.userDataHandling.getUsers.useQuery(
     { userId: user.id },
     {
       enabled: isVisible,
     }
   );
+  const isOverTwoUsers = data && data.length > 2;
 
-  const [emblaRef, emblaApi] = useEmblaCarousel({ draggable: true });
+  useEffect(() => {
+    if (isVisible) void refetch();
+  }, [isVisible]);
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    draggable: true,
+    startIndex: 1,
+  });
 
   return (
     <div
       ref={ref}
-      className="mb-10 mt-2 flex w-full items-center justify-between"
+      className={`mb-10 mt-2 flex w-full items-center justify-center ${
+        data?.length !== 0 && "h-[270.38px]"
+      } ${isOverTwoUsers && "justify-between"}`}
     >
-      <>
-        {data && data.length > 2 && (
+      {!isLoading && (
+        <>
           <button onClick={() => emblaApi?.scrollPrev()}>
-            <SlArrowLeft size={"40px"} className="text-dark" />
+            {isOverTwoUsers && (
+              <SlArrowLeft size={"40px"} className="text-dark" />
+            )}
           </button>
-        )}
-        <div className="w-[90%] flex justify-between">
-          <div className="w-full overflow-hidden" ref={emblaRef}>
-            <div className="flex w-full gap-10">
-              {data?.map((user, index) => {
-                return (
-                  <div
-                    key={index}
-                    className="flex-[0_0_33%] bg-[#d6d6d6] flex min-w-0 flex-col items-center justify-center rounded-2xl py-5"
-                  >
-                    <h3 className="font-orkneyBold">
-                      {roleNameMapping[user.role]}
-                    </h3>
-                    <h2 className="text-2xl">{user.name}</h2>
-                    <div>
-                      {Object.keys(avivableProvision).map((provision) => {
-                        return (
-                          <div key={provision} className="flex gap-2">
-                            <h3>{avivableProvision[provision]}:</h3>
-                            <h3 className="font-semibold">
-                              {user[provision as FeesNames]}
-                            </h3>
-                          </div>
-                        );
-                      })}
+          <div className="flex w-[90%] justify-between">
+            <div className="w-full overflow-hidden" ref={emblaRef}>
+              <div className="flex w-full gap-10">
+                {data?.map((user, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className={`flex min-w-0 flex-[0_0_45%] flex-col items-center justify-center rounded-2xl bg-[#d6d6d6] py-5 xl:flex-[0_0_33%]`}
+                    >
+                      <h3 className="font-orkneyBold">
+                        {roleNameMapping[user.role]}
+                      </h3>
+                      <h2 className="text-2xl">{user.name}</h2>
+                      <div>
+                        {Object.keys(avivableProvision).map((provision) => {
+                          return (
+                            <div key={provision} className="flex gap-2">
+                              <h3>{avivableProvision[provision]}:</h3>
+                              <h3 className="font-semibold">
+                                {user[provision as FeesNames]}
+                              </h3>
+                            </div>
+                          );
+                        })}
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
           </div>
-        </div>
-        {data && data.length > 2 && (
           <button onClick={() => emblaApi?.scrollNext()}>
-            <SlArrowRight size={"40px"} className="text-dark" />
+            {isOverTwoUsers && (
+              <SlArrowRight size={"40px"} className="text-dark" />
+            )}
           </button>
-        )}
-      </>
+        </>
+      )}
     </div>
   );
 };
